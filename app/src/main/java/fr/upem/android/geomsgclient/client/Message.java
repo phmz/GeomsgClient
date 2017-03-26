e8d8f0de194566a58bcefa667f183cab4a368cf5
jramsa <jerome.ramsamy@hotmail.fr>
2017-03-26 14:36:24 +0200

    add  : planifiée, new constructor

--- app/src/main/java/fr/upem/android/geomsgclient/client/Message.java
+++ app/src/main/java/fr/upem/android/geomsgclient/client/Message.java
@@ -12,12 +12,21 @@ public class Message {
     private MessageStatus status;
     private final int userId;
     private final Date messageTime;
+    private boolean planified;
 
     public Message(String message, MessageStatus status, int userId, Date messageTime) {
         this.message = message;
         this.status = status;
         this.userId = userId;
         this.messageTime = messageTime;
+    }
+
+    public Message(String message, MessageStatus status, int userId, Date messageTime, boolean planified) {
+        this.message = message;
+        this.status = status;
+        this.userId = userId;
+        this.messageTime = messageTime;
+        this.planified=planified;
     }
 
     public MessageStatus getStatus() {
@@ -48,5 +57,13 @@ public class Message {
                 ", userId=" + userId +
                 ", messageTime=" + messageTime +
                 '}';
+    }
+
+    public boolean isPlanified() {
+        return planified;
+    }
+
+    public void setPlanified(boolean planified) {
+        this.planified = planified;
     }
 }
